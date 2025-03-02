e784ee3802c17e3ead874e1d9148daf8e93b0ff3
jhansarmiento <jhancsn1@gmail.com>
2025-03-02 17:52:45 -0500

    Creates the router and the controller, and connects it with the server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
+import projectRoutes from './routes/projectRoutes'
 
 // Load environment variables
 dotenv.config()
@@ -8,5 +9,8 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+/* Routes */
+app.use('/api/projects', projectRoutes)
 
 export default app
